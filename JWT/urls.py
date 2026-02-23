bad1423e21698a6a9bd8ff99431b9ab6543b9188
user <user@MacBook-Air--Abdumannon-Rabbimqulov.local>
2026-02-23 16:29:30 +0500

    jwt

--- JWT/urls.py
+++ JWT/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterView
+from .views import RegisterView,Login
 
 urlpatterns = [
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/',RegisterView.as_view())
+    path('register/',RegisterView.as_view()),
+    path('login/',Login.as_view())
 ]
--- JWT/views.py
+++ JWT/views.py
@@ -1,9 +1,12 @@
+from http.client import responses
+
 from .models import User
 from .serializers import UserSerializers,RegisterSerializers
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.authentication import authenticate
 
 class RegisterView(APIView):
     def post(self,request):
@@ -16,8 +19,24 @@ class RegisterView(APIView):
             'data':user.username
         }
         return Response(response)
-class LoginView(APIView):
+
+
+class Login(APIView):
     def post(self,request):
         username=self.request.data.get('username')
         password=self.request.data.get('password')
+
+        user=authenticate(username=username,password=password)
+
+        if not user:
+            return Response({'error':'Parol yoki username xato'})
+        refresh_token=RefreshToken.for_user(user=user)
+
+        response={
+            'status':status.HTTP_200_OK,
+            'message':'login qildiz',
+            'refresh_token':str(refresh_token),
+            'access':str(refresh_token.access_token)
+        }
+        return Response(response)
 
