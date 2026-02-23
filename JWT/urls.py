8e2c7339ca9027968de722e6500d59f241fb465d
user <user@MacBook-Air--Abdumannon-Rabbimqulov.local>
2026-02-23 17:43:02 +0500

    jwt

--- JWT/urls.py
+++ JWT/urls.py
@@ -3,11 +3,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterView,Login
+from .views import RegisterView,Login,Logout
 
 urlpatterns = [
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/',RegisterView.as_view()),
-    path('login/',Login.as_view())
+    path('login/',Login.as_view()),
+    path('logout/',Logout.as_view())
 ]
--- JWT/views.py
+++ JWT/views.py
@@ -1,5 +1,8 @@
 from http.client import responses
 
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
+
 from .models import User
 from .serializers import UserSerializers,RegisterSerializers
 from rest_framework.response import Response
@@ -39,4 +42,39 @@ class Login(APIView):
             'access':str(refresh_token.access_token)
         }
         return Response(response)
+
+
+class Logout(APIView):
+    permission_classes = [AllowAny]
+    def post(self,request):
+        try:
+            refresh_token=self.request.data.get('refresh_token')
+            token=RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'message':'Logout qilindi'},status=status.HTTP_200_OK)
+        except Exception:
+            return Response({'error':'Token xato'},status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
